84a9eb963788bd57a689ccd2fb329b63c308ea82
Maxim Shlemin <clatto@yandex.ru>
2024-09-21 10:47:26 +0300

    Added test for DragAndDrop on site https://the-internet.herokuapp.com/drag_and_drop

---
+++ src/test/java/dragAndDropTest.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class dragAndDropTest {
+    @BeforeAll
+    static void setUp () {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void replaceElementTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        // Проверка перетаскивание квадрата А к квадрату B в правую сторону
+        actions().moveToElement($("#column-a"))
+                .clickAndHold().moveToElement($("#column-b"))
+                .release().perform();
+        $("#column-b").shouldHave(text("A"));
+
+        // Проверка перетаскивание квадрата А к квадрату B в левую сторону
+        actions().moveToElement($("#column-b"))
+                .clickAndHold().moveToElement($("#column-a"))
+                .release().perform();
+        $("#column-a").shouldHave(text("A"));
+    }
+}
